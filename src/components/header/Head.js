64b3f673c5639f2ffa66c2dc46e86c2105f54d30
PerdonCode <niels.perdon1@gmail.com>
2023-06-21 17:37:27 +0200

    show div when click on card button

--- src/components/header/Head.js
+++ src/components/header/Head.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Head.module.css";
 import CartIcon from "../../icons/CartIcon";
+import ShowCard from "../Cart/ShowCard"
 
 const Head = () => {
   const count = 0;
+   const [showCard, setShowsCard] = useState(false);
+   const handleOnClick = () =>{
+    setShowsCard(true);
+   }
 
+   const handleCloseButton = () => {
+    setShowsCard(false);
+   }
   return (
     <header className={styles.container}>
       <h1>ReactMeals</h1>
       <div className={styles.cartButton}>
-        <button>
+        <button onClick={handleOnClick}>
           <CartIcon  style="height: 1px"/> 
           <span>Your Cart</span>
           <p>{count}</p>
         </button>
-      </div>
+
+        
+      </div>{showCard
+        && <div className={showDiv}><ShowCard/>
+        <button onClick={handleCloseButton}>close</button></div>}
     </header>
   );
 };
